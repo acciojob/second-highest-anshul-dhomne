be46909dc5011ca52036d80d2cd0e0a39b7da683
anshul-dhomne <anshul.dhomnee@gmail.com>
2025-04-28 13:39:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let max = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > max) {
+      second = max;
+      max = num;
+    } else if (num > second && num < max) {
+      second = num;
+    }
+  }
+
+  // If second is still -Infinity, all elements are same or there was only one unique value
+  return second === -Infinity ? -Infinity : second;
+}
+
